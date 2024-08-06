1822a58198511ede5fa63c39f16fa3cf9468588b
mrx7014 <safen894@gmail.com>
2024-08-07 02:12:17 +0300

    DT - V1.0.0

---
+++ customize.sh
@@ -0,0 +1,89 @@
+ui_print "
+* Start Applying Tweaks
+"
+sleep 2
+
+ui_print "
+- Improves audio and video recording quality...
+"
+sleep 1
+ui_print "
+- Better call voice quality...
+"
+sleep 1
+ui_print "
+- Faster boot time...
+"
+sleep 1
+ui_print "
+- Better Responsiveness & Speed...
+"
+sleep 1
+ui_print "
+- Zygote preforking...
+"
+sleep 1
+ui_print "
+- FPS Stabilizer...
+"
+sleep 1
+ui_print "
+- Audio Improvements...
+"
+sleep 1
+ui_print "
+- Disables sending of usage data...
+"
+sleep 1
+ui_print "
+- Makes apps load faster and frees more ram...
+"
+sleep 1
+ui_print "
+- Increase jpg quality to 100%...
+"
+sleep 1
+ui_print "
+- Dalvik Virtual Machine tweaks...
+"
+sleep 1
+ui_print "
+- Make Phone Ring as soon as you get a Call...
+"
+sleep 1
+ui_print "
+- Save Battery Without Performance Drop...
+"
+sleep 1
+ui_print "
+- Change Default Rendering Driver To Vulkan...
+"
+sleep 1
+ui_print "
+- Disables blackscreen issue after a call ended...
+"
+sleep 1
+ui_print "
+- Better RAM Management...
+"
+sleep 1
+ui_print "
+- Disable Hungry GMS...
+"
+sleep 1
+ui_print "
+- Aggressive Ram Killer (Prop)...
+"
+sleep 1
+ui_print "
+- Stop Send Logs...
+"
+sleep 1
+ui_print " 
+- DT installation Done
+"
+sleep 1
+ui_print " 
+- join our channel MRX7014 Cloud https://t.me/mrx7014cloud
+"
+sleep 5
