7d0b1789b08d277be685a33f6a30ff87f0607325
MRX7014 | Octopus <safen894@gmail.com>
2024-08-07 12:55:44 +0300

    DT - Device Tweaker V2.0.0 - Stable

--- customize.sh
+++ customize.sh
@@ -2,15 +2,6 @@ ui_print "
 * Start Applying Tweaks
 "
 sleep 2
-
-ui_print "
-- Improves audio and video recording quality...
-"
-sleep 1
-ui_print "
-- Better call voice quality...
-"
-sleep 1
 ui_print "
 - Faster boot time...
 "
@@ -20,15 +11,7 @@ ui_print "
 "
 sleep 1
 ui_print "
-- Zygote preforking...
-"
-sleep 1
-ui_print "
 - FPS Stabilizer...
-"
-sleep 1
-ui_print "
-- Audio Improvements...
 "
 sleep 1
 ui_print "
@@ -40,27 +23,11 @@ ui_print "
 "
 sleep 1
 ui_print "
-- Increase jpg quality to 100%...
-"
-sleep 1
-ui_print "
-- Dalvik Virtual Machine tweaks...
-"
-sleep 1
-ui_print "
-- Make Phone Ring as soon as you get a Call...
-"
-sleep 1
-ui_print "
 - Save Battery Without Performance Drop...
 "
 sleep 1
 ui_print "
 - Change Default Rendering Driver To Vulkan...
-"
-sleep 1
-ui_print "
-- Disables blackscreen issue after a call ended...
 "
 sleep 1
 ui_print "
